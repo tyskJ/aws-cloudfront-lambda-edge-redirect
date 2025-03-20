5cfb565642bffd1ccc3d7e6fe999da26f1af947c
tyskJ <193552101+tyskJ@users.noreply.github.com>
2025-03-20 22:54:59 +0900

    change code

--- code/cdk-app/lib/assets/redirect.py
+++ code/cdk-app/lib/assets/redirect.py
@@ -4,9 +4,9 @@ def lambda_handler(event, context):
 
     # Pathチェック
     if request_path == '/test1/':
-        redirect_url = 'https://www.sub.mtdevelop.click/test1/'
+        redirect_url = 'https://リダイレクト先FQDN/test1/'
     elif request_path == '/test2/':
-        redirect_url = 'https://www.sub.mtdevelop.click/test2/'
+        redirect_url = 'https://リダイレクト先FQDN/test2/'
     else:
         # マッチしない場合は元のリクエストを返す
         return request
